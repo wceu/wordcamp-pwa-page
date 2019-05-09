da2faf828edb7575d01663bafb8c4177a659f2a8
Antonio Villegas <lotendil@gmail.com>
2019-05-09 17:17:27 +0200

    Use _embed in sessions endpoint to only make 2 requests instead of 3. Speakers are now embedded in sessions.

--- templates/assets/pwa-page.js
+++ templates/assets/pwa-page.js
@@ -6,15 +6,13 @@ processAndRenderData = ( data ) => {
 
 init = () => {
 	Promise.all([
-		fetch( apiRoot + '/wp/v2/sessions?status=publish&per_page=100' ).then( response => response.json() ),
-		fetch( apiRoot + '/wp/v2/speakers?status=publish&per_page=100' ).then( response => response.json() ),
+		fetch( apiRoot + '/wp/v2/sessions?status=publish&_embed=true&per_page=100' ).then( response => response.json() ),
 		fetch( apiRoot + '/wp/v2/session_track?status=publish&per_page=100' ).then( response => response.json() ),
 	])
 	.then( data => {
 		processAndRenderData( {
 			sessions: data[0],
-			speakers: data[1],
-			tracks: data[2],
+			tracks: data[1],
 		} );
 	})
 	.catch( error => {
