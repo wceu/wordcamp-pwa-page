6f60d954a0344922b9679945b96dcdf52ed2ef18
tfrommen <info@tfrommen.de>
2019-05-10 13:41:22 +0200

    Simplify webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,12 @@
  */
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
-module.exports = [
-
-	{
-		...defaultConfig,
-		entry: {
-			'pwa-page': './templates/assets/pwa-page',
-		},
-		output: {
-			filename: '[name].js',
-		},
+module.exports = {
+	...defaultConfig,
+	entry: {
+		'pwa-page': './templates/assets/pwa-page',
 	},
-
-];
-
+	output: {
+		filename: '[name].js',
+	},
+};
