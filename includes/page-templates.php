c802c6484c5adffe5e6d73640cf7a381bc205bc4
tfrommen <info@tfrommen.de>
2019-05-16 01:35:43 +0200

    Simplify template functions.

--- includes/page-templates.php
+++ includes/page-templates.php
@@ -10,62 +10,78 @@
 
 namespace WordCamp\PWAPage\Templates;
 
-add_filter( 'theme_page_templates', __NAMESPACE__ . '\\add_new_template' );
-add_filter( 'template_include',     __NAMESPACE__ . '\\view_project_template' );
+/**
+ * File name of the PWA Homepage template.
+ *
+ * @var string
+ */
+const TEMPLATE_PWA_HOMEPAGE = 'template-pwa-home.php';
+
+add_filter( 'theme_page_templates', __NAMESPACE__ . '\\add_pwa_homepage_template' );
+add_filter( 'template_include', __NAMESPACE__ . '\\use_pwa_homepage_template' );
 add_action( 'wp_enqueue_scripts',   __NAMESPACE__ . '\\enqueue_assets' );
 
 /**
- * Adds template to the attributes metabox.
+ * Adds the PWA Homepage template.
+ *
+ * @param string[] $page_templates Page templates.
+ *
+ * @return string[] Page templates.
  */
-function add_new_template( array $posts_templates ) {
-	$posts_templates = array_merge(
-		$posts_templates,
-		[
-			'template-pwa-home.php' => 'PWA Style Homepage',
-		]
+function add_pwa_homepage_template( array $page_templates ) {
+	return array_merge(
+		$page_templates,
+		[ TEMPLATE_PWA_HOMEPAGE => 'PWA Style Homepage' ]
 	);
-	return $posts_templates;
 }
 
 /**
- * Determines if the page has our template assigned and return it's path.
+ * Load the PWA Homepage template file.
+ *
+ * @param string $page_template Current page template.
+ *
+ * @return string Page template.
  */
-function view_project_template( string $template ) {
-	global $post;
-
-	if ( ! is_current_page_using_template( 'template-pwa-home.php' ) ) {
-		return $template;
+function use_pwa_homepage_template( string $page_template ) {
+	if ( is_pwa_homepage_template() ) {
+		$pwa_homepage_template = WCPWAP_PLUGIN_PATH . '/templates/' . TEMPLATE_PWA_HOMEPAGE;
+		if ( is_readable( $pwa_homepage_template ) ) {
+			return $pwa_homepage_template;
+		}
 	}
 
-	$file = WCPWAP_PLUGIN_PATH . '/templates/' . get_post_meta( $post->ID, '_wp_page_template', true );
-
-	// Just to be safe, we check if the file exists and is readable first.
-	if ( is_readable( $file ) ) {
-		return $file;
-	}
-
-	return $template;
+	return $page_template;
 }
 
 /**
  * Enqueues CSS and JS assets for our page template.
  */
 function enqueue_assets() {
-	if ( is_current_page_using_template( 'template-pwa-home.php' ) ) {
-		wp_enqueue_script( 'wordcamp-pwa-page', WCPWAP_PLUGIN_URL . '/dist/pwa-page.js', [ 'lodash' ], filemtime( WCPWAP_PLUGIN_PATH . '/dist/pwa-page.js' ), true );
-		wp_enqueue_style( 'wordcamp-pwa-page', WCPWAP_PLUGIN_URL . '/templates/assets/pwa-page.css', [], filemtime(  WCPWAP_PLUGIN_PATH . '/templates/assets/pwa-page.css' ) );
+	if ( ! is_pwa_homepage_template() ) {
+		return;
 	}
+
+	wp_enqueue_script(
+		'wordcamp-pwa-page',
+		WCPWAP_PLUGIN_URL . '/dist/pwa-page.js',
+		[ 'lodash' ],
+		filemtime( WCPWAP_PLUGIN_PATH . '/dist/pwa-page.js' ),
+		true
+	);
+
+	wp_enqueue_style(
+		'wordcamp-pwa-page',
+		WCPWAP_PLUGIN_URL . '/templates/assets/pwa-page.css',
+		[],
+		filemtime(  WCPWAP_PLUGIN_PATH . '/templates/assets/pwa-page.css' )
+	);
 }
 
 /**
- * Helper function to determine whether the current page is using our template.
+ * Helper function to determine if the current page is using the PWA Homepage template.
+ *
+ * @return bool Whether or not the current page is using the PWA Homepage template.
  */
-function is_current_page_using_template( string $template_file ) {
-	global $post;
-
-	if ( ! isset( $post ) ) {
-		return false;
-	}
-
-	return ( get_post_meta( $post->ID, '_wp_page_template', true ) === $template_file );
+function is_pwa_homepage_template() {
+	return get_post_meta( get_the_ID(), '_wp_page_template', true ) === TEMPLATE_PWA_HOMEPAGE;
 }
