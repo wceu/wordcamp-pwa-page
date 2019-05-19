5da5b112d5ab951a83884c3e2730969d984bc6fe
Antonio Villegas <lotendil@gmail.com>
2019-05-19 13:14:39 +0100

    Render latest posts in page template

--- templates/assets/pwa-page.js
+++ templates/assets/pwa-page.js
@@ -6,7 +6,7 @@ const apiRoot = 'https://2019.europe.wordcamp.org/wp-json';
 // TODO get current date instead of hardcoded one.
 const now = new Date( '2019-06-22T11:35:00Z' );
 
-const processAndRenderData = ( data ) => {
+const processAndRenderScheduleData = ( data ) => {
 
 	const tracks = data.tracks.map( ( track ) => {
 		const sessionsInTrack = _.sortBy(
@@ -31,10 +31,10 @@ const processAndRenderData = ( data ) => {
 			next: nextSession,
 		};
 	} );
-	render( tracks );
+	renderSchedule( tracks );
 };
 
-const render = ( tracks ) => {
+const renderSchedule = ( tracks ) => {
 
 	const onNow = document.getElementById( 'on-now' );
 	const upNext = document.getElementById( 'up-next' );
@@ -52,12 +52,20 @@ const renderSession = ( session, track, container ) => {
 	sessionContainer.setAttribute( 'class', 'wordcamp-schedule-session' );
 
 	renderSessionTitle( session, sessionContainer );
+	renderSessionTime( session, sessionContainer );
 	renderSessionTrack( track, sessionContainer );
 	renderSessionSpeakers( session, sessionContainer );
 	renderSessionCategory( session, sessionContainer );
 
 	container.appendChild( sessionContainer );
 
+};
+
+const renderSessionTime = ( session, container ) => {
+	const time = document.createElement( 'span' );
+	time.setAttribute( 'class', 'wordcamp-schedule-session-time' );
+	time.innerHTML = session.session_date_time.time;
+	container.appendChild( time );
 };
 
 const renderSessionTitle = ( session, container ) => {
@@ -115,16 +123,88 @@ const renderSessionCategory = ( session, container ) => {
 
 };
 
+const renderLatestPosts = ( posts ) => {
+	const container = document.getElementById( 'latest-posts' );
+
+	posts.forEach( ( post ) => {
+		renderPost( post, container );
+	} );
+};
+
+const renderPost = ( post, container ) => {
+	const postContainer = document.createElement( 'div' );
+	postContainer.setAttribute( 'class', 'wordcamp-latest-post' );
+
+	renderPostDate( post, postContainer );
+	renderPostTitle( post, postContainer );
+	renderPostExcerpt( post, postContainer );
+	renderPostCategories( post, postContainer );
+
+	container.appendChild( postContainer );
+};
+
+const renderPostDate = ( post, container ) => {
+	const postDate = document.createElement( 'span' );
+	postDate.setAttribute( 'class', 'wordcamp-latest-post-date' );
+	postDate.innerHTML = new Date( post.date_gmt ).toLocaleDateString();
+	container.appendChild( postDate );
+};
+
+const renderPostTitle = ( post, container ) => {
+
+	const title = document.createElement( 'h4' );
+	title.setAttribute( 'class', 'wordcamp-latest-post-title' );
+
+	const titleLink = document.createElement( 'a' );
+	titleLink.setAttribute( 'href', post.link );
+	titleLink.innerHTML = post.title.rendered;
+
+	title.appendChild( titleLink );
+	container.appendChild( title );
+
+};
+
+const renderPostExcerpt = ( post, container ) => {
+	const excerpt = document.createElement( 'span' );
+	excerpt.setAttribute( 'class', 'wordcamp-latest-post-excerpt' );
+	excerpt.innerHTML = post.excerpt.rendered;
+	container.appendChild( excerpt );
+};
+
+const renderPostCategories = ( post, container ) => {
+
+	const categories = document.createElement( 'span' );
+	categories.setAttribute( 'class', 'wordcamp-latest-post-categories' );
+
+	const terms = _.keyBy( _.flatten( post._embedded[ 'wp:term' ] ), 'id' );
+	if ( post.categories.length > 0 ) {
+		post.categories.forEach( ( c ) => {
+			const term = terms[ c ];
+
+			const category = document.createElement( 'a' );
+			category.setAttribute( 'href', term.link );
+			category.innerHTML = term.name;
+
+			categories.appendChild( category );
+		} );
+	}//end if
+
+	container.appendChild( categories );
+
+};
+
 const init = () => {
 	Promise.all( [
 		fetch( apiRoot + '/wp/v2/sessions?status=publish&_embed=true&per_page=100' ).then( ( response ) => response.json() ),
 		fetch( apiRoot + '/wp/v2/session_track?status=publish&per_page=100' ).then( ( response ) => response.json() ),
+		fetch( apiRoot + '/wp/v2/posts?status=publish&_embed=true&per_page=3' ).then( ( response ) => response.json() ),
 	] )
 		.then( ( data ) => {
-			processAndRenderData( {
+			processAndRenderScheduleData( {
 				sessions: data[ 0 ],
 				tracks: data[ 1 ],
 			} );
+			renderLatestPosts( data[ 2 ] );
 		} )
 		.catch( ( error ) => {
 			console.log( 'Error: ', error );
--- templates/template-pwa-home.php
+++ templates/template-pwa-home.php
@@ -20,11 +20,22 @@ namespace WordCamp\PWAPage\Templates;
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 	<h1><?php echo esc_html_x( 'Live Schedule', 'title', 'wordcamp-pwa-page' ); ?></h1>
+
 	<h2><?php echo esc_html_x( 'On now', 'title', 'wordcamp-pwa-page' ); ?></h2>
 	<div id="on-now" class="pwa-page-content"></div>
 
 	<h2><?php echo esc_html_x( 'Up next', 'title', 'wordcamp-pwa-page' ); ?></h2>
 	<div id="up-next" class="pwa-page-content"></div>
+
+	<a href="<?php echo esc_url( site_url( __( 'schedule', 'wordcamp-pwa-page' ) ) ); ?>" class="full-schedule">
+		<?php echo esc_html_x( 'View Full Schedule', 'text', 'wordcamp-pwa-page' ); ?>
+	</a>
+
+	<h2><?php echo esc_html_x( 'Latest Posts', 'title', 'wordcamp-pwa-page' ); ?></h2>
+	<div id="latest-posts" class="pwa-page-content"></div>
+	<a href="<?php echo esc_url( get_post_type_archive_link( 'post' ) ); ?>" class="all-posts">
+		<?php echo esc_html_x( 'View All Posts', 'text', 'wordcamp-pwa-page' ); ?>
+	</a>
 <?php wp_footer(); ?>
 </body>
 </html>
