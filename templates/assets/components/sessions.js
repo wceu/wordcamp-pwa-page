17614fb2be4702688eff23ec054b93a64a8b53ad
Koen Van den Wijngaert <koen@koenvandenwijngaert.be>
2019-06-13 08:09:02 +0200

    Improved readability for SessionsGroup component

--- templates/assets/components/sessions.js
+++ templates/assets/components/sessions.js
@@ -5,5 +5,11 @@ import { Session } from './session';
 
 export const SessionsGroup = ( { title, sessions } ) => <>
 	<h3>{ title }</h3>
-	{ sessions.filter( ( session ) => !! session ).map( ( session ) => <Session key={ `${ session.session.id }-${ session.track.id }` } session={ session } /> ) }
+	{ sessions.filter( ( session ) => !! session ).map( ( session ) =>
+		<Session
+			key={ `${ session.session.id }-${ session.track.id }` }
+			session={ session }
+		/>
+	)
+	}
 </>;
